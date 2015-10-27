556948ea2d2e3c4bd1e1941a2a2efa0de2524058
Scott Wagner <jwagne12@villanova.edu>
2015-10-26 21:49:45 -0400

    updated to use correct variable

--- js/http-response-tester.js
+++ js/http-response-tester.js
@@ -2,7 +2,7 @@ var app = require('./app.js');
 
 var server = app.listen(process.env.PORT || 3000, function() {
     var address = server.address(),
-        host = address.host,
+        host = address.address,
         port = address.port;
 
     console.log('http-response-tester listening at http://%s:%s', host, port);
