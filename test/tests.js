428097cbc2d3c738868bfcc3f2b10c3f6cfb19b3
Scott Wagner <jwagne12@villanova.edu>
2015-10-26 21:23:22 -0400

    added test coverage for common http status codes

--- test/tests.js
+++ test/tests.js
@@ -1,17 +1,24 @@
-var chai = require('chai');
-var supertest = require('supertest');
-var app = require('../js/app.js');
+var chai = require('chai'),
+    supertest = require('supertest'),
+    app = require('../js/app.js');
+
+var commonStatusCodes = [200, 302, 400, 401, 404, 500, 502, 503];
 
 describe('Responder', function() {
     describe('Valid Request', function() {
-        it('should respond 200 when requested', function(done) {
-            supertest(app).get('/200')
-                .expect(200)
-                .end(function (err) {
-                    done(err);
-                });
+        commonStatusCodes.forEach(function(statusCode) {
+            it('should respond ' + statusCode + ' when requested', function (done) {
+                supertest(app).get('/' + statusCode)
+                    .expect(statusCode)
+                    .end(function (err, res) {
+                        console.log(res.statusCode + '\n');
+                        done(err);
+                    });
+            });
         });
+    });
 
+    describe('Invalid Request', function() {
         it('should respond 400 when an invalid request is made', function(done) {
             supertest(app).get('/invalidRequest')
                 .expect(400)
